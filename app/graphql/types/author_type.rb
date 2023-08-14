5c5fdd4309c133fb657cd28bcc6c0a59633e0b91
Cleiviane Costa <costa.cleiviane@gmail.com>
2023-08-14 20:25:41 -0300

    Camelize a specific field

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -3,7 +3,7 @@ module Types
     description "One author"
     
     field :id, ID, null: false
-    field :first_name, String, null: true
+    field :first_name, String, null: true, camelize: false
     field :last_name, String, null: true
     field :yob, Int, null: false
     field :is_alive, Boolean, null: true
