b01ddf3cc72d820c2294ab113efd595763e93301
Cleiviane Costa <costa.cleiviane@gmail.com>
2023-08-31 10:37:33 -0300

    Define custom field for the types

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -3,10 +3,15 @@ module Types
     description "One author"
     
     field :id, ID, null: false
-    field :first_name, String, null: true, camelize: false
-    field :last_name, String, null: true
+    field :first_name, String, null: false
+    field :last_name, String, null: false
     field :yob, Int, null: false
     field :is_alive, Boolean, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :full_name, String, null: false
+
+    def full_name
+      "#{object.first_name} #{object.last_name}"
+    end
   end
 end
