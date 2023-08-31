245a6721a0cb99e7035c3e4dfd4bd25eb0b281a4
Cleiviane Costa <costa.cleiviane@gmail.com>
2023-08-31 12:14:30 -0300

    Define a custom type to get the coordinates

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -1,17 +1,20 @@
 module Types
   class AuthorType < Types::BaseObject
     description "One author"
-    
+
     field :id, ID, null: false
     field :first_name, String, null: false
     field :last_name, String, null: false
     field :yob, Int, null: false
     field :is_alive, Boolean, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+
     field :full_name, String, null: false
 
     def full_name
       "#{object.first_name} #{object.last_name}"
     end
+
+    field :coordinates, Types::CoordinatesType, null: false
   end
 end
